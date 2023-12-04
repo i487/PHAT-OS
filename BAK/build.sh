01478933720e29f4292102ed0653ff256a50f456
i487 <atd_ata56k@proton.me>
2023-12-04 13:33:22 +0000

    Removed unused files

--- BAK/build.sh
+++
@@ -1,6 +0,0 @@
-nasm -i /home/dann/osTst/src src/bootsec.asm -o build/bootsec.bin
-nasm -i /home/dann/osTst/src src/boot.asm -o build/boot.bin
-
-dd if=/dev/zero of=build/floppy.img bs=512 count=2880
-mkfs.fat -F 12 -n "FS" build/floppy.img
-dd if=build/bootsec.bin of=build/floppy.img conv=notrunc
--- BAK/test.sh
+++
@@ -1,2 +0,0 @@
-bash build.sh
-qemu-system-i386 -fda build/disk.bin -monitor stdio
